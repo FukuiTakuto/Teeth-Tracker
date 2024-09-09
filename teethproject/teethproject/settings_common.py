55221445304bfac55e10228d484262460042a499
fukuitakuto <fortune.comes.291tkt@ozzio.jp>
2024-09-10 00:30:10 +0900

    STATIC_ROOTを設定

--- teethproject/teethproject/settings_common.py
+++ teethproject/teethproject/settings_common.py
@@ -77,6 +77,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = '/home/fukky/static/'
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
